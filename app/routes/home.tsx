46b2bf42762427462fc4a51410ae52159471fad7
mrezayusufy <yousofimreza@gmail.com>
2026-02-09 04:29:53 +0330

    Products list created and rendered by map and onPurchase function passed to ProductCard

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,7 @@
 import type { Route } from "./+types/home";
-import { ProductCard } from "~/components";
+import { ProductCard, ProductList } from "~/components";
 
-export function meta({}: Route.MetaArgs) {
+export function meta({ }: Route.MetaArgs) {
   return [
     { title: "New React Router App" },
     { name: "description", content: "Welcome to React Router!" },
@@ -9,5 +9,44 @@ export function meta({}: Route.MetaArgs) {
 }
 
 export default function Home() {
-  return <ProductCard />;
+  const products = [
+    {
+      image: "images/iphone.png",
+      title: "iPhone 15 Pro",
+      spec: [
+        "A17 Pro chip with 6-core GPU",
+        "3x or 5x Telephoto camera",
+        "Up to 29 hours video playback",
+      ],
+      price: 999,
+    },
+    {
+      image: "images/airpods.png",
+      title: "AirPods Pro 2",
+      spec: [
+        "Noise Cancellation",
+        "Dust, sweat, and water resistant",
+        "Up to 6 hours of listening",
+      ],
+      price: 249,
+    },
+    {
+      image: "images/apple-watch.png",
+      title: "Apple Watch 9",
+      spec: [
+        "45mm or 41mm case size",
+        "Always-On Retina display",
+        "Up to 18 hours normal use",
+      ],
+      price: 399,
+    },
+  ];
+  const onPurchase = (product) => {
+    alert(`You clicked on ${product.title} which cost $${product.price}`);
+  }
+  return <ProductList>
+    {products.map((item) => (
+      <ProductCard product={item} onPurchase={onPurchase} />
+    ))}
+  </ProductList>;
 }
